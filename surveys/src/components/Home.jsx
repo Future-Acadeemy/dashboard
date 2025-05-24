d8c4922c59c792227f7cb50d344a098c5bb4b087
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-05-24 13:50:07 +0300

    filter

--- surveys/src/components/Home.jsx
+++ surveys/src/components/Home.jsx
@@ -39,33 +39,36 @@ const Home = () => {
       {error && <p className="text-lg text-red-600">{error}</p>}
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-3xl">
-        {clients.map((user) => (
-          <div
-            key={user._id}
-            className="bg-white p-6 rounded-lg shadow-lg cursor-pointer hover:bg-blue-100 transition"
-          >
-            <h2 className="text-2xl font-semibold text-gray-800">
-              {user.name}
-            </h2>
-            <p className="text-gray-600">Phone: {user.phone}</p>
-            <p className="text-gray-600">Age: {user.age}</p>
-            <p className="text-gray-600">Gender: {user.gender}</p>
-            <p className="text-gray-600">Job: {user.job}</p>
-            <p
-              className={`font-semibold ${
-                user.isLoggedIn ? "text-green-600" : "text-red-600"
-              }`}
+        {/* {clients.map((user) => ( */}
+        {clients
+          .filter((user) => user.job === "Central Finance Department")
+          .map((user) => (
+            <div
+              key={user._id}
+              className="bg-white p-6 rounded-lg shadow-lg cursor-pointer hover:bg-blue-100 transition"
             >
-              {user.isLoggedIn ? "Online" : "Offline"}
-            </p>
-            <button
-              onClick={() => navigate(`/userSurvey/${user.phone}`)}
-              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
-            >
-              View Details
-            </button>
-          </div>
-        ))}
+              <h2 className="text-2xl font-semibold text-gray-800">
+                {user.name}
+              </h2>
+              <p className="text-gray-600">Phone: {user.phone}</p>
+              <p className="text-gray-600">Age: {user.age}</p>
+              <p className="text-gray-600">Gender: {user.gender}</p>
+              <p className="text-gray-600">Job: {user.job}</p>
+              <p
+                className={`font-semibold ${
+                  user.isLoggedIn ? "text-green-600" : "text-red-600"
+                }`}
+              >
+                {user.isLoggedIn ? "Online" : "Offline"}
+              </p>
+              <button
+                onClick={() => navigate(`/userSurvey/${user.phone}`)}
+                className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
+              >
+                View Details
+              </button>
+            </div>
+          ))}
       </div>
     </div>
   );
